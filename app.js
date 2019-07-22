e019f4827ded2d5e41a359da10a1bea5373905cf
riyadh <riyadh02220@gmail.com>
2019-07-22 17:57:50 +0200

    fade out arrows when klick more

--- app.js
+++ app.js
@@ -2,24 +2,30 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 
 const more = document.querySelector("#more");
-const left = document.querySelector("#more");
+const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 
 const logo = document.querySelector("#logo");
 const standart = document.querySelector("#standart");
 const artistName = document.querySelector("#artistName");
 
+// FADE OUT ARROWS WHEN CLICK THE MORE BUTTON :
 more.addEventListener("click",()=> {
-    moveOut();
+    setTimeout(()=> {
+        moveOut();
+    },0);
+    setTimeout(()=> {
+        fadeIt();
+    },200);
 });
 
+// FUNCTIONS : 
 function moveOut() {
-    /*main.style.padding = "0px 0px";*/
-    footer.style.alignItems = "flex-end";
-
-    /*
-    
-    left.style.float = "left";
-    right.style.float = "right";
-    */
+        main.style.padding = "0px 0px";
+        footer.style.alignItems = "flex-end";
+}
+function fadeIt() {
+        left.style.visibility =  "hidden";
+        right.style.visibility =  "hidden";
+        more.style.visibility =  "hidden";
 }
